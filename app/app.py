0f0af7950339c9829ddbce90e6c9e7509d02d3f1
Bernardo Morales <bernardomorales@Bernardos-iMac.local>
2025-11-01 09:57:51 +0100

    Fix CORS: Add PATCH method for tier updates

--- app/app.py
+++ app/app.py
@@ -19,7 +19,7 @@ CORS(app, resources={
             "http://localhost:3000",
             "http://127.0.0.1:3000"
         ],
-        "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        "methods": ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True
     }
